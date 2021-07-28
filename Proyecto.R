eb36f9abbae89977f4e984a3a4e2db2e1276f9ab
[yanelyluna] <[yanelyluna@ciencias.unam.mx]>
2021-07-28 17:16:03 -0500

    set up de los datos y el análisis exploratorio

---
+++ Proyecto.R
@@ -0,0 +1,41 @@
+################# PROYECTO FINAL DE ESTADÍSTICA BAYESIANA #################
+#-------------------------- 2021-2 ---------------------------------------
+#--- Librerías ----
+library(rjags)
+library(ggplot2)
+library(tidyverse)
+# Colores
+colores <- c("#00afbb","#ff5044") #Verde: 0, Rojo: 1
+options(digits = 3)
+
+#--- Datos ----
+SAheart <- read.table("http://www-stat.stanford.edu/~tibs/ElemStatLearn/datasets/SAheart.data",
+                      sep=",",head=T,row.names=1)
+
+str(SAheart) # 462 observaciones de 10 variables
+which(is.na(SAheart)) # No hay datos faltantes
+
+# Convertimos a factor la variable respuesta
+SAheart$chd <- factor(SAheart$chd)
+
+# Convertimos a factores las variables categóricas
+SAheart$famhist <- factor(SAheart$famhist)
+
+#--- Análisis Descriptivo ----
+#---- Correlación entre variables
+pairs(SAheart[,-c(5,10)],col=colores[SAheart$chd]) #Verde: 0, Rojo: 1
+cor(SAheart[,-c(5,10)])
+# La correrlación más alta es entre adiposity y obesity
+
+
+#---- Análisis del efecto de las variables sdp, tobacco, ldl, adiposity, famhist 
+# en la variable respuesta chd
+
+
+
+#---- Análisis del efecto de las variables typea, obesity, alcohol, age
+# en la variable respuesta chd
+
+
+#----- Ajuste del modelo con glm ---------------
+
